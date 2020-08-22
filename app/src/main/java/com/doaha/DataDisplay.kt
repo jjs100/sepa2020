0b507331ee0c4b89d7ace6c094e153b4009d197b
Julian <zaren99@gmail.com>
2020-08-22 23:28:38 +1000

    [Julian Lia][Data Display Updates 2]

--- app/src/main/java/com/doaha/DataDisplay.kt
+++ app/src/main/java/com/doaha/DataDisplay.kt
@@ -1,4 +1,36 @@
 package com.doaha
 
-class DataDisplay {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.doaha.SplashScreen.Companion.EXTRA_MESSAGE
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.data_display.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
+
+
+class DataDisplay : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.data_display)
+        val zoneId = intent.getStringExtra(EXTRA_MESSAGE)
+        getInfo(zoneId)
+    }
+
+    private fun getInfo(zoneId: String) {
+        val docRef = FirebaseFirestore.getInstance().collection("zones").document(zoneId)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(1000L)
+            val region = docRef.get().await()
+            Picasso.get().load(region.getString("img1")).into(image1)
+            Picasso.get().load(region.getString("img2")).into(image2)
+            Picasso.get().load(region.getString("img3")).into(image3)
+            welcomeText.text = region.getString("Welcome").toString()
+            ackText.text = "Acknowledgments: " + region.getString("Acknowledgements")
+            infoText.text = region.getString("Info").toString()
+        }
+    }
 }
--- app/src/main/java/com/doaha/SplashScreen.kt
+++ app/src/main/java/com/doaha/SplashScreen.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.splash_layout.*
 import android.content.Intent
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,42 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+
+        //LocationSpinner Implementation
+        val spinner: Spinner = findViewById(R.id.LocationSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.zones_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = this
+    }
+    private var itemSelect = false
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        itemSelect = true
+        parent.getItemAtPosition((pos))
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        itemSelect = false
+    }
+
+    fun searchButton(v: View) {
+        if (itemSelect){
+            val id: String = LocationSpinner.selectedItem.toString()
+            val intent = Intent(this, DataDisplay::class.java).apply {
+                putExtra(EXTRA_MESSAGE, id)
+            }
+            startActivity(intent)
+        }
+
+    }
+
+    companion object {
+            const val EXTRA_MESSAGE = ""
     }
 }
+
