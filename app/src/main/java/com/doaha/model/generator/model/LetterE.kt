ec0195fa6536460915aec8a7d24fe47b0cac2eed
jjs100 <jord_harry@hotmail.com>
2020-10-18 17:57:36 +1100

    [Jordan Smith][Nation Labling before Charater Lib Created]

--- app/src/main/java/com/doaha/model/generator/model/LetterE.kt
+++ app/src/main/java/com/doaha/model/generator/model/LetterE.kt
@@ -8,27 +8,72 @@ class LetterE (centerPoint: LatLng, offset: Int) : Character(centerPoint, offset
         //1
         jsonArrayChild.put(funCreateLatLngPoint(startPointLat, startPointLong))
         //2
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat, startPointLong+longLine))
+        jsonArrayChild.put(funCreateLatLngPoint(startPointLat, startPointLong + longLine))
         //3
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat + 2*longLine/5, startPointLong+longLine))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 2 * longLine / 5,
+                startPointLong + longLine
+            )
+        )
         //4
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 2*longLine/5, startPointLong+2*longLine/3))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 2 * longLine / 5,
+                startPointLong + 2 * longLine / 3
+            )
+        )
         //5
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 4*longLine/5, startPointLong+2*longLine/3))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 4 * longLine / 5,
+                startPointLong + 2 * longLine / 3
+            )
+        )
         //6
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 4*longLine/5, startPointLong+longLine))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 4 * longLine / 5,
+                startPointLong + longLine
+            )
+        )
         //7
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 6*longLine/5, startPointLong+longLine))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 6 * longLine / 5,
+                startPointLong + longLine
+            )
+        )
         //8
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 6*longLine/5, startPointLong+2*longLine/3))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 6 * longLine / 5,
+                startPointLong + 2 * longLine / 3
+            )
+        )
         //9
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 8*longLine/5, startPointLong+2*longLine/3))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 8 * longLine / 5,
+                startPointLong + 2 * longLine / 3
+            )
+        )
         //10
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 8*longLine/5, startPointLong+longLine))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 8 * longLine / 5,
+                startPointLong + longLine
+            )
+        )
         //11
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 2*longLine, startPointLong+longLine))
+        jsonArrayChild.put(
+            funCreateLatLngPoint(
+                startPointLat + 2 * longLine,
+                startPointLong + longLine
+            )
+        )
         //12
-        jsonArrayChild.put(funCreateLatLngPoint(startPointLat+ 2*longLine, startPointLong))
+        jsonArrayChild.put(funCreateLatLngPoint(startPointLat + 2 * longLine, startPointLong))
         //13
         jsonArrayChild.put(funCreateLatLngPoint(startPointLat, startPointLong))
         jsonArrayParent.put(jsonArrayChild)
